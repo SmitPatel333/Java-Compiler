bc1cad026266ea28beeb3865217a53a09c74c8da
Smit Patel <36279476+SmitPatel333@users.noreply.github.com>
2018-07-11 12:15:32 -0400

    Update Lexer.java

--- src/Front-End/Lexer.java
+++ src/Front-End/Lexer.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-public class Blankboard {
+public class Lexer {
 	private static enum Types{
 		Identifier, Keyword, Separator, Operator, Literal 
 	}
