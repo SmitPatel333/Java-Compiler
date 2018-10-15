bfb7b733309b033a5b1227afc1a8073c3c07100c
Smit Patel <36279476+SmitPatel333@users.noreply.github.com>
2018-10-15 14:40:29 -0400

    Add HashMap for Action Table

--- src/Grammar/Grammar.java
+++ src/Grammar/Grammar.java
@@ -1,5 +1,8 @@
 package Grammar;
+import java.util.Hashmap;
 
 public class Grammar {
-
+  
+  private HashMap<Integer, HashMap<String, String>> action_table = new Hashmap<Integer, HashMap<String, String>>();
+  private HashMap<String, String> column = new HashMap<String, String>();
 }
