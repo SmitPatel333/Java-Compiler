5d12332be8d8c27a39254a5066dfc4d4c0f04949
Smit Patel <Smitypatel03@yahoo.com>
2018-12-03 17:25:43 -0500

    Update Grammar.java

--- src/Grammar/Grammar.java
+++ src/Grammar/Grammar.java
@@ -1,8 +1,18 @@
 package Grammar;
+
 import java.util.Hashmap;
 import extra_files.Pair;
 
 public class Grammar {
+  protected HashMap<Pair, String> action_table = new Hashmap<Pair, String>();
   
-  private HashMap<Pair, String> action_table = new Hashmap<Pair, String>();
+  public class production_Rule {
+	  private String left;
+	  private String right;
+	  
+	  public production_Rule(String left, String right) {
+		  this.left = left;
+		  this.right = right;
+	  }
+  }
 }
